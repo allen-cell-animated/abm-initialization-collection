e7b0a6b828ebdd5f779d683de52209a027849df8
jessicasyu <jesyu@uw.edu>
2022-10-27 18:28:38 -0400

    Update docstring for scale sample coordinates task

--- src/abm_initialization_collection/sample/scale_sample_coordinates.py
+++ src/abm_initialization_collection/sample/scale_sample_coordinates.py
@@ -1,11 +1,44 @@
+from typing import Optional
+
 from prefect import task
 import pandas as pd
 
 
 @task
 def scale_sample_coordinates(
-    samples: pd.DataFrame, coordinate_type: str, resolution: float, scale_xy: float, scale_z: float
+    samples: pd.DataFrame,
+    coordinate_type: Optional[str],
+    resolution: float,
+    scale_xy: float,
+    scale_z: float,
 ) -> pd.DataFrame:
+    """
+    Scales sampled coordinates using to given coordinate type.
+
+    The "absolute" coordinate type scales sample index coordinate into absolute
+    positions (in um).
+    The "step" coordinate type scales sample index coordinates by step size.
+    Otherwise, samples index coordinates are not modified.
+
+    Parameters
+    ----------
+    samples
+        Sample cell ids and coordinates.
+    coordinate_type : {'absolute', 'step', None}
+        The coordinate scaling type.
+    resolution
+        Distance between samples (um).
+    scale_xy
+        Resolution scaling in x/y (um/pixel).
+    scale_z
+        Resolution scaling in z (um/pixel).
+
+    Returns
+    -------
+    :
+        Sample cell ids and scaled coordinates.
+    """
+
     if coordinate_type == "absolute":
         samples["x"] = samples["x"] * scale_xy
         samples["y"] = samples["y"] * scale_xy
