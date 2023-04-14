495c32bd31a5a07eb8e80cbe424c56e2ea8f53dd
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-04-14 11:33:21 -0400

    Update select fov images task to use bins

--- src/abm_initialization_collection/image/select_fov_images.py
+++ src/abm_initialization_collection/image/select_fov_images.py
@@ -1,20 +1,26 @@
+import numpy as np
 import pandas as pd
 
 
-def select_fov_images(metadata: pd.DataFrame, cells_per_fov: int, num_fovs: int) -> list[dict]:
-    fov_count = 0
-    fov_seg_paths = []
+def select_fov_images(
+    metadata: pd.DataFrame, cells_per_fov: int, bins: list[int], counts: list[int]
+) -> list[dict]:
+    total_count = sum(counts)
+    bin_counts = [0] * len(counts)
+    fovs = []
 
     for fov_seg_path, seg_group in metadata.groupby("fov_seg_path"):
-        no_outliers = (seg_group.outlier == "No").all()
+        no_outliers = (seg_group.outlier != "Yes").all()
         only_m0 = (seg_group.cell_stage == "M0").all()
 
-        if no_outliers and only_m0:
-            if len(seg_group) == cells_per_fov:
-                fov_count = fov_count + 1
-                key = fov_seg_path.split("/")[-1].split("_")[0]
+        if no_outliers and only_m0 and len(seg_group) == cells_per_fov:
+            mean = seg_group["mem_shape_volume_lcc"].mean()
+            bin_index = np.digitize(mean, bins)
 
-                fov_seg_paths.append(
+            if bin_counts[bin_index] < counts[bin_index - 1]:
+                bin_counts[bin_index] = bin_counts[bin_index] + 1
+                key = fov_seg_path.split("/")[-1].split("_")[0]
+                fovs.append(
                     {
                         "key": key,
                         "item": fov_seg_path,
@@ -22,7 +28,7 @@ def select_fov_images(metadata: pd.DataFrame, cells_per_fov: int, num_fovs: int)
                     }
                 )
 
-        if len(fov_seg_paths) == num_fovs:
-            break
+            if sum(bin_counts) >= total_count:
+                break
 
-    return fov_seg_paths
+    return fovs
